66c6c0b92429a178374cb4f989b988e39f457824
marinamarshall <marinamarshall2@gmail.com>
2021-11-03 11:16:38 +0000

    Arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,43 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum)
+
 // Arrow Function With Parameters
 
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum)
+
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b);
+const addTwoNumbers2 = (a, b) => a + b;
+
+let sum2 = addTwoNumbers(1, 5);
+console.log(sum2)
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+
+const sayHello = () => console.log('Hello'); // Declaring a function with empty parentheses
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
